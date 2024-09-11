99443af8850743ddf7fece73718c67a3091fc75e
BalavikneshRam-Codes <balaram0711@gmail.com>
2024-09-11 21:54:14 +0530

    commit 2

--- leetcode290.java
+++ leetcode290.java
@@ -1,29 +1,25 @@
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class leetcode290 {
  public static void main(String[] args) {
   System.out.println(wordPattern("abba", "dog cat cat dog"));
  } 
  public static boolean wordPattern(String pattern, String s) {
-      String[] arr = s.split(" ");
-      HashMap<Character,String> hash = new HashMap<>();
-
-      for(int i = 0;i < pattern.length();i++){
-        char ch = pattern.charAt(i);
-        boolean isContainsKey = hash.containsKey(ch);
-
-        if(hash.containsValue(arr[i]) && !isContainsKey){
-          return false;
+        HashMap<Character,String> map=new HashMap<>();
+        String[] array=s.split(" ");
+        HashSet<String> set=new HashSet<>();
+        if(pattern.length()!=array.length) return false;
+        for(int i=0;i<pattern.length();i++){
+            if(map.containsKey(pattern.charAt(i))){
+                if(!array[i].equals(map.get(pattern.charAt(i)))) return false;
+            }else{
+                if(set.contains(array[i]))return false;
+                map.put(pattern.charAt(i),array[i]);
+                set.add(array[i]);
+            }
         }
-        if(isContainsKey && !hash.get(ch).equals(arr[i])){
-          return false;
-        }
-
-        else{
-          hash.put(ch, arr[i]);
-        }
-      }
-      return true;
+        return true;
 
  }
 }
